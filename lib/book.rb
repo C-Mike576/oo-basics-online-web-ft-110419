d754efd293b7d8c05605003a3d45717d11273113
Michael Calkins <m.calkins576@gmail.com>
2019-11-06 21:01:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,6 +17,14 @@ class Book
   def page_count
     @page_count
   end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+  end
 end
 
 Book.new("And Then There Were None")
